6b2a1e923b2b62386ea87597545cf681071dce33
Longlasting1805 <Longlasting1805@gmail.com>
2025-05-24 15:05:36 +0100

    Initial commit for admin

---
+++ src/assets/assets.js
@@ -0,0 +1,20 @@
+import diamondlogo from './diamondlogo.jpg'
+import add_icon from './add_icon.jpg'
+import order_icon from './order_icon.jpg'
+import upload_area from './upload_area.jpg'
+import percel_icon from './percel_icon.jpg'
+
+
+
+
+
+
+
+export const assets = {
+    diamondlogo, 
+    add_icon,
+    order_icon,
+    upload_area,
+    percel_icon
+    
+}
